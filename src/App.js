9affa2b91364f6a9109bbb221e8d9d7a549813b4
vin-vu <vince.hvu@gmail.com>
2021-06-11 16:16:32 -0700

    Created custom useInterval hook and displayed last updated

--- src/App.js
+++ src/App.js
@@ -1,20 +1,51 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useRef} from 'react';
 import axios from 'axios';
 import './App.css';
 import Coin from './Coin.js';
+
+function useInterval(callback, delay) {
+  const savedCallback = useRef();
+
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval.
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+}
 
 function App() {
 
   const [coins, setCoins] = useState([]);
   const [search, setSearch] = useState('');
+  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false';
 
-  useEffect(() => {
-    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+  // useEffect(() => {
+  //   axios.get(url)
+  //   .then(res => {
+  //     setCoins(res.data);
+  //   })
+  //   .catch(error => console.log(error));
+  // }, []);
+
+  useInterval(() => {
+    axios.get(url)
     .then(res => {
       setCoins(res.data);
     })
     .catch(error => console.log(error));
-  }, []);
+    },
+    5000
+  )
 
   const handleChange = e => {
     setSearch(e.target.value);
@@ -22,7 +53,12 @@ function App() {
 
   const filteredCoins = coins.filter(coin => 
     coin.name.toLowerCase().includes(search.toLowerCase())
-  );
+  )
+
+  // let date = new Date(filteredCoins[0]);
+  let date = new Date(filteredCoins[0].last_updated);
+  let localDate = date.toString().slice(0, 25)
+  console.log(localDate)
 
   return (
     <div className="coin-app">
@@ -32,6 +68,12 @@ function App() {
           <input type="text" className="coin-input" placeholder="Search" onChange={handleChange}></input> 
         </form>
       </div>
+      <div className="last-updated">
+        Last Updated: {localDate}
+      </div>
+      {/* <div className='last-updated'>
+        {filteredCoins[0].last_updated}
+      </div> */}
       <table className="labels">
         <th className="name">Name</th>
         <th className="symbol">Symbol</th>
@@ -40,6 +82,7 @@ function App() {
         <th className="volume">Volume(24hr)</th>
         <th className="market-cap">Market Cap</th>
       </table>
+    
       {filteredCoins.map(coin => {
         return (
           <Coin 
